f924ad6e014a8cc698fc8b1a74fc9e7eeaa3539a
kirieis <144522943+kirieis@users.noreply.github.com>
2026-07-22 17:40:29 +0700

    save esp8266

--- docs/wokwi contruct/sketch.ino
+++ docs/wokwi contruct/sketch.ino
@@ -9,7 +9,11 @@
   ====================================================================
 */
 
-#include <ESP8266WiFi.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#else
+  #include <WiFi.h>
+#endif
 #include <PubSubClient.h>
 #include <Wire.h>
 #include <RTClib.h>
